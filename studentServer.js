7192b586511961615b1f8132a8ff999709fa5ad0
Brianna Haeckl <breee.haeckl@gmail.com>
2019-03-18 20:12:28 -0400

    Add body-parser to be able to process uploaded data

--- studentServer.js
+++ studentServer.js
@@ -3,6 +3,7 @@ var express=require('express');
 var path=require("path");
 var app=express();
 
+
 //add an http server to serve files to the Edge browser
 //due to certificate issues it rejects the https files if they are not
 //directly called in a typed URL
@@ -15,11 +16,25 @@ app.get('/',function(req,res){
 });
 
 
+var bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({
+	extended:true
+}));
+app.use(bodyParser.json());
+
+
 //adding functionality to log the requests
 app.use(function(req,res,next){
 	var filename=path.basename(req.url);
 	var extension=path.extname(filename);
 	console.log("The file " +filename+ " was requested.");
+	next();
+});
+
+var app=express();
+app.use(function(req,res,next){
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	next();
 });
 

f75dda4a5133ce876fadc15eb639f970b07d72b9
Brianna Haeckl <breee.haeckl@gmail.com>
2019-03-19 23:00:22 -0400

    Adds get function to extract the formdata that we have uploaded

--- studentServer.js
+++ studentServer.js
@@ -101,6 +101,38 @@ var querystring=querystring + geometrystring + ")";
 });
 
 
+
+app.get('/getFormData/:port_id', function(req,res){
+	pool.connect(function(err,client,done){
+		if(err){
+			console.log("not able to get connection " + err);
+			res.status(400).send(err);
+		}
+		//use the inbuilt geoJSON functionality
+		//and create the required geoJSON format using a query adapted from here:
+		//http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html
+		//note that query needs to be a single string with no line breaks so build it up bit by bit
+
+ var querystring = " SELECT 'FeatureCollection' As type,array_to_json(array_agg(f)) As features FROM ";
+ querystring = querystring + "(SELECT 'Feature' As type ,ST_AsGeoJSON(lg.geom)::json As geometry, ";
+ querystring = querystring + "row_to_json((SELECT l FROM (SELECT name,surname, port_id) As l ";
+ querystring = querystring + " )) As properties";
+ querystring = querystring + " FROM formdata As lg where lg.port_id= '"+req.params.port_id + "' limit 100 ) As f ";
+ console.log(querystring);
+ client.query(querystring,function(err,result){ 
+
+			//call done() to release the client back to the pool
+			done();
+			if(err){
+				console.log(err);
+				res.status(400).send(err);
+			}
+			res.status(200).send(result.rows);
+		});
+	});
+});
+
+
 //adding functionality to log the requests
 app.use(function(req,res,next){
 	var filename=path.basename(req.url);
@@ -108,8 +140,6 @@ app.use(function(req,res,next){
 	console.log("The file " +filename+ " was requested.");
 	next();
 });
-
-
 
 
 //serve static files-e.g. html, css
